441aa3c120a2de6e5d60ac20a7d8795c146fb5af
rajshish333 <rajshish333@gmail.com>
2023-12-19 13:28:13 +0530

    f

--- src/component/AllRoutes.jsx
+++ src/component/AllRoutes.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Rajshishdcs from "./rajshish/Rajshishdcs";
 import Home from "./home/Home";
 import Contact from "./contact/Contact";
 import MyworkPage from "../page/mywork/MyworkPage";
