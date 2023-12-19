0719ce21e88134c97fdcbb33ee110f46be35a7ff
rajshish333 <rajshish333@gmail.com>
2023-12-19 21:26:50 +0530

    l

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,17 @@
-import './App.css'
-import Navbar from './component/navbar/Navbar';
-import Footer from './component/footer/Footer';
-import AllRoutes from './component/AllRoutes';
-import { BrowserRouter } from 'react-router-dom';
+import "./App.css";
+import Navbar from "./component/navbar/Navbar";
+import Footer from "./component/footer/Footer";
+import AllRoutes from "./component/AllRoutes";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-
   return (
-    <div className="main-container">
     <BrowserRouter>
       <Navbar />
-        <AllRoutes /> 
+      <AllRoutes />
       <Footer className="footer" />
     </BrowserRouter>
-  </div>
-  )
+  );
 }
 
-export default App
-
+export default App;
--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -7,17 +7,18 @@ const Home = () => {
     <div className="container-home">
       <div className="logo">
         <img src={devloper} alt="Developer" className="developer-image" />
-        <span className="heading-raj">Hi! I'm Rajshish Singh </span>
-        <p className="mydata">
-          I'm a passionate developer enthusiastic about building robust and
-          scalable web applications using modern technologies.
-        </p>
+        <div className="text-container">
+          <span className="heading-raj">Hi! I'm Rajshish Singh </span>
+          <p className="mydata">
+            I'm a passionate developer enthusiastic about building robust and
+            scalable web applications using modern technologies.
+          </p>
+        </div>
       </div>
       <div className="display-flex">
-          <button href="RajshishFr.pdf" download="RajshishFr.pdf">
-            Download Resume
-          </button>
-        
+        <a href="RajshishFr.pdf" download="RajshishFr.pdf">
+          <button>Download Resume</button>
+        </a>
       </div>
     </div>
   );
