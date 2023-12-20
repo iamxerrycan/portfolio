02c8e81e60f4d0692d3896f47e10e0792397a1ab
rajshish333 <rajshish333@gmail.com>
2023-12-20 11:43:47 +0530

    l

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -14,7 +14,7 @@
 //       </Link>
 //       <Link to="/About">
 //         <a id="navText">About</a>
-     
+
 //     </span>
 //   );
 // };
@@ -25,7 +25,7 @@ import React, { useState } from "react";
 import "./navbar.css";
 import { Link } from "react-router-dom";
 import logo from "../../assets/devloper.png";
-import { FaBars } from "react-icons/fa"; 
+import { FaBars } from "react-icons/fa";
 
 const Navbar = () => {
   const [showNavbar, setShowNavbar] = useState(false);
@@ -50,17 +50,16 @@ const Navbar = () => {
         </Link>
         <Link to="/About">
           <a id="navText">About</a>
-        
         </Link>
-      <Link to="/Skill">
-        <a id="navText">Skill</a>
-      </Link>
-      <Link to="/MyworkPage">
-        <a id="navText">Work</a>
-      </Link>
-      <Link to="/Contact">
-        <a id="navText">Contact</a>
-      </Link>
+        <Link to="/Skill">
+          <a id="navText">Skill</a>
+        </Link>
+        <Link to="/MyworkPage">
+          <a id="navText">Work</a>
+        </Link>
+        <Link to="/Contact">
+          <a id="navText">Contact</a>
+        </Link>
       </div>
     </div>
   );
