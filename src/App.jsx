6b9faa2693dbc034895c10e968b5a3f54f959590
rajshish333 <rajshish333@gmail.com>
2023-12-20 12:06:33 +0530

    l

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,21 @@ function App() {
     <BrowserRouter>
       
       <Navbar />
-      <ToastContainer/>
+      <ToastContainer    position="top-center"
+        autoClose={3000}
+        hideProgressBar={false}
+        closeOnClick
+        pauseOnHover
+        draggable
+        progress={undefined}
+        style={{
+          backgroundColor: 'rgba(0, 0, 0, 0.8)',
+          color: 'green',
+          fontSize: '14px',
+          maxWidth: '300px',
+          maxHeight:"50px",
+          borderRadius: "20px"
+        }}/>
       <AllRoutes />
       <Footer className="footer" />
     </BrowserRouter>
