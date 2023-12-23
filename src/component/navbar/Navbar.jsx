64c7dc504064a322e2fc7157d711b4b478d3722f
rajshish333 <rajshish333@gmail.com>
2023-12-23 17:17:06 +0530

    Navbar Update

--- src/component/navbar/Navbar.jsx
+++ src/component/navbar/Navbar.jsx
@@ -1,26 +1,3 @@
-// import React from "react";
-// import "./navbar.css";
-// import { Link } from "react-router-dom";
-// import logo from "../../assets/devloper.png";
-
-// const Navbar = () => {
-//   return (
-//     <span className="navbarDiv">
-//       <Link to="/">
-//         <img id="mylogo" src={logo} alt="" />
-//       </Link>
-//       <Link to="/">
-//         <a id="navText">Home</a>
-//       </Link>
-//       <Link to="/About">
-//         <a id="navText">About</a>
-
-//     </span>
-//   );
-// };
-
-// export default Navbar;
-
 import React, { useState } from "react";
 import "./navbar.css";
 import { Link } from "react-router-dom";
@@ -37,9 +14,9 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <div className="navbar-header">
-        <Link to="/">
+        {/* <Link to="/"> */}
           <img id="mylogo" src={logo} alt="" />
-        </Link>
+        {/* </Link> */}
         <div className="toggle-icon" onClick={toggleNavbar}>
           <FaBars />
         </div>
