aa682b030e071d0771395b658b067ef92e9ad4d4
rajshish333 <rajshish333@gmail.com>
2024-02-02 19:59:34 +0530

    fix

--- src/assets/data/MyworkData.jsx
+++ src/assets/data/MyworkData.jsx
@@ -1,46 +1,52 @@
-const MyworkData =[
-    {
-        name: "User Management (CRUD)",
-        contributors: ["Rajshish"],
-        technologies: ["React", "Node.js", "express","API"],
-        link: "https://www.linkedin.com/posts/activity-7144168109698916352-aof2?utm_source=share&utm_medium=member_android"
-      },
-      {
-        name: "SignUp-LoginIn",
-        contributors: ["Rajshish"],
-        technologies: ["React", "Redux", "Api-Integration"],
-        link: "https://sign-up-in-auth.netlify.app"
-      },
-    {
-        name: "Counter",
-        contributors: ["Rajshish"],
-        technologies: ["React", "Hooks","state"],
-        link: "https://webappcounter.netlify.app"
-      },
-      {
-        name: "Portfollio",
-        contributors: ["Rajshish"],
-        technologies: ["React", "Js","Toast"],
-        link: "https://github.com/rajshish333/portfolio"
-      },
-      {
-        name: "The Unknown",
-        contributors: ["Rajshish"],
-        technologies: ["html","css", "Js"],
-        link: "https://iamxerrycan.netlify.app"
-      }, {
-        name: "Caclulator",
-        contributors: ["Rajshish"],
-        technologies: ["Reactjs"],
-        link: "https://calculator-react-webapp.netlify.app/"
-      },{
-        name: "Dhwani",
-        contributors: ["Rajshish"],
-        technologies: ["Reactjs "],
-        link: "https://dhvanee.netlify.app/"
-      }
-    
-    
-]
+const MyworkData = [
+  {
+    name: "User Management (CRUD)",
+    contributors: ["Rajshish"],
+    technologies: ["React", "Node.js", "express", "API"],
+    link: "https://www.linkedin.com/posts/activity-7144168109698916352-aof2?utm_source=share&utm_medium=member_android",
+  },
+  {
+    name: "SignUp-LoginIn",
+    contributors: ["Rajshish"],
+    technologies: ["React", "Redux", "Api-Integration"],
+    link: "https://sign-up-in-auth.netlify.app",
+  },
+  {
+    name: "Counter",
+    contributors: ["Rajshish"],
+    technologies: ["React", "Hooks", "state"],
+    link: "https://webappcounter.netlify.app",
+  },
+  {
+    name: "Portfollio",
+    contributors: ["Rajshish"],
+    technologies: ["React", "Js", "Toast"],
+    link: "https://github.com/iamxerrycan/portfolio",
+  },
+  {
+    name: "The Unknown",
+    contributors: ["Rajshish"],
+    technologies: ["html", "css", "Js"],
+    link: "https://iamxerrycan.netlify.app",
+  },
+  {
+    name: "Caclulator",
+    contributors: ["Rajshish"],
+    technologies: ["Reactjs"],
+    link: "https://calculator-react-webapp.netlify.app/",
+  },
+  {
+    name: "Dhwani",
+    contributors: ["Rajshish"],
+    technologies: ["Reactjs "],
+    link: "https://dhvanee.netlify.app/",
+  },
+  {
+    name: "NGO project",
+    contributors: ["Rajshish"],
+    technologies: ["Reactjs "],
+    link: "https://github.com/iamxerrycan/ngo",
+  },
+];
 
 export default MyworkData;
